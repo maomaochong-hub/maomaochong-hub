7b35dd01345e7450b98450b6fee9683938fdbe7e
maomaochong <maomao_chong18@qq.com>
2026-07-13 22:17:47 +0800

    首次提交

---
+++ Plural_Conversion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void Plural_Conservation(double a, double b, double rho, double theta)
+{
+    rho = sqrt(a * a + b* b);
+
+}
+
+int main()
+{
+
+    return 0;
+}
